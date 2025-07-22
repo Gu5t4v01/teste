ab12d6e2fe1b6f75ea8987c58322265ec9457ccb
Gu5t4v01 <gustsam@gmail.com>
2025-07-22 19:06:24 -0300

    Conserto ortografica

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -7,7 +7,7 @@ let tentativas = 1;
 
 
 while (guess != secreto) {
-   guess = prompt('Escolha um numerro de 0 a ' + numeroMaximo);
+   guess = prompt('Escolha um número de 0 a ' + numeroMaximo);
 //Se o chete for igual
     if (guess == secreto) {
         break;
@@ -15,14 +15,14 @@ while (guess != secreto) {
     //Se for diferente
     } else {
         if (guess < secreto) {
-            alert('Numero secreto é maior que ' + guess);
+            alert('Número secreto é maior que ' + guess);
         } else {
-            alert('Numero secreto é menor que ' + guess);
+            alert('Número secreto é menor que ' + guess);
         }
         
     } tentativas++;
 } 
 
 let palavraTentativa = tentativas > 1 ? ' tentativas!!' : ' tentativa!!'
-alert('Parabens você acertou ' + secreto + ' com ' + tentativas + palavraTentativa);
+alert('Parabéns você acertou ' + secreto + ' com ' + tentativas + palavraTentativa);
 
